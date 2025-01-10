8395d9c6acb89014da56fea4a5e9cef7b545e34c
Luz <ladsan@hotmail.com>
2025-01-10 15:18:13 -0500

    add github origin

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://ladsan1977.github.io"
     ],
     allow_methods=["POST"],
     allow_headers=["Content-Type"],
